49dd42051a897da56c9154a15ab6bf529e250aab
Hemath24 <36098169+Hemath24@users.noreply.github.com>
2023-01-13 02:40:48 +0530

    Update algorithm.bat

--- algorithm.bat
+++ algorithm.bat
@@ -1,1 +1,23 @@
-echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+@echo off
+:: Print Fibonacci Numbers
+:: https://helloacm.com
+
+title Fibonacci Numbers
+
+setlocal enableextensions enabledelayedexpansion
+set n=15
+
+set a=1
+set b=1
+
+set s= %a% %b%
+for /l %%g in (1,1,%n%) do (
+	set /a c=a+b
+	set s=!s! !c!
+	set /a a=b
+	set /a b=c
+)
+
+echo !s!
+tee report.txt
+endlocal
